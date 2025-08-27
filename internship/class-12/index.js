9b2957e6fe01d80611dc357c87e9f83a27a2e95a
Neha Sharma <147707969+Neha12Sharma@users.noreply.github.com>
2025-08-27 20:30:51 +0530

    Add files via upload

---
+++ internship/class-12/index.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const { v4: uuidv4 } = require('uuid');
+const PORT = 4444;
+
+app.use(express.json()); // axios sends the json data
+app.use(express.urlencoded({ extended: true })); // post request by default 
+app.use(express.static(path.join(__dirname, 'public'))); // public folder ko bhejega
+
+let Todos = [
+    // {
+    //     id:"";
+    //     task:"something",
+    //     completed:false
+    // }
+];
+
+app.post("/add-todo",(req,res)=>{
+    const task = req.body.task;
+    Todos.push({task:task,completed:false,id:uuidv4()});
+    res.status(201).json({Todos});
+})
+
+app.delete("/todo/:id",(req,res)=>{
+    const id = req.params.id;
+    Todos = Todos.filter((todo)=>{
+        return todo.id != id;
+    })
+    res.status(200).json({Todos});
+})
+
+app.put("/todo/:id",(req,res)=>{
+    const id = req.params.id;
+    Todos = Todos.map((todo)=>{
+        if(todo.id == id){
+            return {
+                ...todo,
+                completed:!(todo.completed)
+            }
+        }
+        return todo;
+    })
+    res.status(200).json({Todos});
+})
+
+app.delete("/clear-complete",(req,res)=>{
+    Todos = Todos.filter((todo)=>{
+        return todo.completed==false;
+    })
+    res.status(200).json({Todos});
+})
+
+app.get("/todo/filter",(req,res)=>{
+    const filter = req.query.filter;
+    console.log(req.query);
+    if(filter=="active"){
+        const filteredTodos = Todos.filter((todo)=>{
+            return todo.completed == false;
+        })
+        return res.status(200).json({Todos:filteredTodos})
+    }
+    if(filter=="completed"){
+        const filteredTodos = Todos.filter((todo)=>{
+            return todo.completed == true;
+        })
+        return res.status(200).json({Todos:filteredTodos})
+    }
+    res.status(200).json({Todos})
+})
+
+app.get("/all-todos",(req,res)=>{
+    res.status(200).json({Todos})
+})
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:` + PORT);
+});
